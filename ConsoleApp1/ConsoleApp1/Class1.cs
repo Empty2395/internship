5ba3221eda4aeb3135de090315f7475668891be4
Roma <romap8654@gmail.com>
2024-07-02 21:59:41 +0300

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public class FileAnalyzer
+{
+    private List<int> numbers;
+
+    public FileAnalyzer(string filePath)
+    {
+        numbers = new List<int>();
+        LoadNumbers(filePath);
+    }
+
+    private void LoadNumbers(string filePath)
+    {
+        var lines = File.ReadAllLines(filePath);
+        foreach (var line in lines)
+        {
+            if (int.TryParse(line, out int number))
+            {
+                numbers.Add(number);
+            }
+        }
+    }
+
+    public int MaxNumber()
+    {
+        return numbers.Max();
+    }
+
+    public int MinNumber()
+    {
+        return numbers.Min();
+    }
+
+    public double Median()
+    {
+        var sortedNumbers = numbers.OrderBy(n => n).ToList();
+        int count = sortedNumbers.Count;
+        if (count % 2 == 0)
+        {
+            return (sortedNumbers[count / 2 - 1] + sortedNumbers[count / 2]) / 2.0;
+        }
+        else
+        {
+            return sortedNumbers[count / 2];
+        }
+    }
+
+    public double Average()
+    {
+        return numbers.Average();
+    }
+
+    public List<int> LongestIncreasingSequence()
+    {
+        if (numbers.Count == 0) return new List<int>();
+
+        List<int> longestSequence = new List<int>();
+        List<int> currentSequence = new List<int> { numbers[0] };
+
+        for (int i = 1; i < numbers.Count; i++)
+        {
+            if (numbers[i] > numbers[i - 1])
+            {
+                currentSequence.Add(numbers[i]);
+            }
+            else
+            {
+                if (currentSequence.Count > longestSequence.Count)
+                {
+                    longestSequence = new List<int>(currentSequence);
+                }
+                currentSequence.Clear();
+                currentSequence.Add(numbers[i]);
+            }
+        }
+
+        if (currentSequence.Count > longestSequence.Count)
+        {
+            longestSequence = currentSequence;
+        }
+
+        return longestSequence;
+    }
+
+    public List<int> LongestDecreasingSequence()
+    {
+        if (numbers.Count == 0) return new List<int>();
+
+        List<int> longestSequence = new List<int>();
+        List<int> currentSequence = new List<int> { numbers[0] };
+
+        for (int i = 1; i < numbers.Count; i++)
+        {
+            if (numbers[i] < numbers[i - 1])
+            {
+                currentSequence.Add(numbers[i]);
+            }
+            else
+            {
+                if (currentSequence.Count > longestSequence.Count)
+                {
+                    longestSequence = new List<int>(currentSequence);
+                }
+                currentSequence.Clear();
+                currentSequence.Add(numbers[i]);
+            }
+        }
+
+        if (currentSequence.Count > longestSequence.Count)
+        {
+            longestSequence = currentSequence;
+        }
+
+        return longestSequence;
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\ACER\Desktop\стажировка\фалй\10m.txt";
+
+            FileAnalyzer analyzer = new FileAnalyzer(filePath);
+
+            Console.WriteLine("1.Максимальне число в файлi: " + analyzer.MaxNumber());
+            Console.WriteLine("2.Мiнiмальне число в файлi: " + analyzer.MinNumber());
+            Console.WriteLine("3.Медiану: " + analyzer.Median());
+            Console.WriteLine("4.Середнє арифметичне значення: " + analyzer.Average());
+
+            var longestIncreasingSequence = analyzer.LongestIncreasingSequence();
+            Console.WriteLine("5.Найбiльша послiдовнiсть чисел, яка збiльшується: " + string.Join(", ", longestIncreasingSequence));
+
+            var longestDecreasingSequence = analyzer.LongestDecreasingSequence();
+            Console.WriteLine("6.Найбiльша послiдовнiсть чисел, яка зменшується: " + string.Join(", ", longestDecreasingSequence));
+            Console.ReadLine();
+        }
+    }
+}
